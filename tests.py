b485b2a0b077c5f8371d333fabcec8ba325dd205
Ukpono Obott <legendsergio@gmail.com>
2022-02-13 21:08:01 +0100

    Added tests

--- tests.py
+++ tests.py
@@ -1,37 +1,26 @@
-from db import db
+# # from db import db
 import unittest
+from datetime import date, timedelta
+from sqlalchemy import false
 from models.user import UserModel
 from models.project import ProjectModel
-from models.task import TaskModel
-from .app import app
+# from models.task import TaskModel
+# from app import app
 
-
-class Tests(unittest.TestCase):
-    def setUp(self) -> None:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-        db.create_all()
-        return "Created"
-        
-
-    def tearDown(self) -> None:
-        db.session.remove()
-        db.drop_all()
+class BaseCase(unittest.TestCase):
 
     def test_registration(self):
         user = UserModel("Ukpono Obott", "ukpono@test.com", confirmed=False, password="strongpassword")
-        res = user.save_to_db()
-        assert res == "OK"
-
-    def test_find_by_email(self):
-        find_user = UserModel.find_by_email("ukpono@test.com")
-
-        assert isinstance(find_user, UserModel)
-
-    def test_find_by_email_no_user(self):
-        find_no_user = UserModel.find_by_email("no_email@test.com")
         
-        assert not find_no_user
+        assert user.full_name == "Ukpono Obott"
+        assert user.email == "ukpono@test.com"
+        assert user.confirmed == False
+        assert user.password_hash != "strongpassword"
 
+    def test_project(self):
+        project = ProjectModel("Linux Deployment", "Deploy a linux server on Azure", 20, "Ukpono")
+        assert project.completed == False
+        assert project.end_date != date.today()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
